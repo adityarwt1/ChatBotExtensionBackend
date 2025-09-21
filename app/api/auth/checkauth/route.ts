899876dc370f7a06e94844405f192e6719d17e81
Aditya Rawat <adityarawatnew2487@gmail.com>
2025-09-21 12:48:13 +0530

    done the chatbot backend comppletly

--- app/api/auth/checkauth/route.ts
+++ app/api/auth/checkauth/route.ts
@@ -1,45 +1,59 @@
 import { corsHeader } from "@/utils/corsHeader";
 import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 export async function POST(req: NextRequest) {
   try {
     const token = (await cookies()).get("chatbot")?.value;
-    console.log('token' , token)
+    console.log("token", token);
 
-    if( !token){
-      return NextResponse.json({error: "token not found"},{status: 401 ,headers : corsHeader})
+    if (!token) {
+      return NextResponse.json(
+        { error: "token not found" },
+        { status: 401, headers: corsHeader }
+      );
     }
 
-    const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as {exp: number}
+    const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as {
+      exp: number;
+    };
 
-    if(!decoded){
-      return NextResponse.json({error: "unable to decode the token"},{status: 401, headers: corsHeader})
+    if (!decoded) {
+      return NextResponse.json(
+        { error: "unable to decode the token" },
+        { status: 401, headers: corsHeader }
+      );
     }
 
-    if(new Date(decoded.exp * 1000) < new Date()){
-      return NextResponse.json({error: "Token expired"},{status: 401, headers: corsHeader})
+    if (new Date(decoded.exp * 1000) < new Date()) {
+      return NextResponse.json(
+        { error: "Token expired" },
+        { status: 401, headers: corsHeader }
+      );
     }
 
-    return NextResponse.json({message: "User Auntheticated"},{status: 200 , headers: corsHeader})
+    return NextResponse.json(
+      { message: "User Auntheticated" },
+      { status: 200, headers: corsHeader }
+    );
   } catch (error) {
     return NextResponse.json(
       { error: (error as Error).message },
-      { status: 500 , headers: corsHeader}
+      { status: 500, headers: corsHeader }
     );
   }
-}   
-
-
-
+}
 
 export async function OPTIONS(req: NextRequest) {
   try {
-    return NextResponse.json({ message: "cors preflight" }, { status: 200  ,headers: corsHeader});
+    return NextResponse.json(
+      { message: "cors preflight" },
+      { status: 200, headers: corsHeader }
+    );
   } catch (error) {
     return NextResponse.json(
       { error: (error as Error).message },
-      { status: 500 , headers: corsHeader }
+      { status: 500, headers: corsHeader }
     );
   }
 }
--- app/api/chatbot/route.ts
+++ app/api/chatbot/route.ts
@@ -3,37 +3,39 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
   try {
-    const {promt} = await req.json()
+    const { promt } = await req.json();
 
-    if(!promt){
-        return NextResponse.json({error: "Prompt not provided"},{status: 400,headers: corsHeader})
+    if (!promt) {
+      return NextResponse.json(
+        { error: "Prompt not provided" },
+        { status: 400, headers: corsHeader }
+      );
     }
- const response = await fetch(
-   "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent",
-   {
-     method: "POST",
-     headers: {
-       "Content-Type": "application/json",
-       "X-goog-api-key": process.env.GEMINI_API_KEY!, // better: keep key in .env
-     },
-     body: JSON.stringify({
-       contents: [
-         {
-           parts: [
-             {
-               text: promt,
-             },
-            
-           ],
-         },
-       ],
-     }),
-   }
- );
+    const response = await fetch(
+      "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "X-goog-api-key": process.env.GEMINI_API_KEY!, // better: keep key in .env
+        },
+        body: JSON.stringify({
+          contents: [
+            {
+              parts: [
+                {
+                  text: promt,
+                },
+              ],
+            },
+          ],
+        }),
+      }
+    );
 
- const data = await response.json();
+    const data = await response.json();
 
- if(response.ok){
+    if (response.ok) {
       if (response.ok) {
         const text =
           data.candidates?.[0]?.content?.parts?.[0]?.text || "No response";
@@ -47,24 +49,26 @@ export async function POST(req: NextRequest) {
           { status: 503, headers: corsHeader }
         );
       }
- }
+    }
   } catch (error) {
     return NextResponse.json(
       { error: (error as Error).message },
-      { status: 500 ,headers: corsHeader }
+      { status: 500, headers: corsHeader }
     );
   }
 }
 
-
-
 export async function OPTIONS(req: NextRequest) {
-
-    try {
-        return NextResponse.json({message: "Cors Preflight"},{status: 200,headers   : corsHeader})
-    } catch (error) {
-        console.log((error as Error).message)
-    return NextResponse.json({error:"Something went wrong"},{status: 500 , headers:corsHeader})
-    }
-    
+  try {
+    return NextResponse.json(
+      { message: "Cors Preflight" },
+      { status: 200, headers: corsHeader }
+    );
+  } catch (error) {
+    console.log((error as Error).message);
+    return NextResponse.json(
+      { error: "Something went wrong" },
+      { status: 500, headers: corsHeader }
+    );
+  }
 }
